2489bbc1e0a139ab6954524ab2d6a1aee7a15008
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-15 16:44:52 -0500

    test suite completion for teamMember object

--- index.js
+++ index.js
@@ -1,10 +1,248 @@
 const inquirer = require('inquirer');
-const fs = require('fs')
+const fs = require('fs');
+const generateHTML = require('./js/makeHTML');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+
+const teamMembers = [] //an array to store team members for now.
+
 
 function managerPrompt (){
+    return inquirer.prompt([
+        {
+          type: 'input',
+          message: 'Who is the manager of this team?',
+          name: 'managerName',
+          validate: filledName => {
+            if(filledName){
 
-}
+                return true
+            }
+            else{
+                console.log("Please enter a name!")
+                return false
 
-function teamMemberPrompt (){
+            }
+          }},
+        {
+          type: 'input',
+          message: 'Please enter the managers ID number',
+          name: 'managerID',
+          validate: numInput =>{
+            if(isNaN(numInput)){
+                console.log("Please enter a valid ID number!")
+                return false
+            }
+            else{
+                return true
+
+            }
+
+          }
+        },
+        {
+          type: 'input',
+          message: 'Please input the managers email',
+          name: 'managerEmail',
+          validate: validEmail => {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(validEmail)    //taken from a Stackoverflow post
+            
+            if(valid){
+                console.log("Email accepted")
+                return true
+
+            }
+            else{
+                console.log("Please enter a valid email!")
+                return false
+
+            }
+
+          }
+
+        },
+        {
+            type: 'input',
+            message: 'Please input the managers office number',
+            name: 'managerOffice',
+            validate: officeNumber => {
+                if(isNaN(officeNumber)){
+                    console.log("Please enter a valid office number")
+                    return false
+
+                }
+                else{
+                    return true
+
+                }
+
+            }
+          },
+        ])
+        .then(managerInput => {
+            const  { managerName, managerID, managerEmail, managerOffice } = managerInput; 
+            const manager = new Manager (managerName, managerID, managerEmail, managerOffice);
     
-}
+            teamMembers.push(manager); 
+            console.log(manager); 
+        })
+    };    
+    
+    
+    
+
+
+        function teamMemberPrompt (){
+            return inquirer.prompt([
+
+                {
+                    type: 'list',
+                    message: 'Is this an engineer or an intern?',
+                    name: 'jobRank',
+                    choices: ["Engineer", "Intern"]
+                    },
+                {
+                  type: 'input',
+                  message: 'What is this teamMembers name?',
+                  name: 'teamMemberName',
+                  validate: filledName => {
+                    if(filledName){
+        
+                        return true
+                    }
+                    else{
+                        console.log("Please enter a name!")
+                        return false
+        
+                    }
+                  }},
+                {
+                  type: 'input',
+                  message: 'Please input the teamMembers email',
+                  name: 'teamMemberEmail',
+                  validate: validEmail => {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(validEmail)   
+                    
+                    if(valid){
+                        console.log("Email accepted")
+                        return true
+        
+                    }
+                    else{
+                        console.log("Please enter a valid email!")
+                        return false
+        
+                    }
+        
+                  }
+        
+                },
+
+                {
+                    type: 'input',
+                    name: 'teamMemberID',
+                    message: "Please enter the teamMember's ID.",
+                    validate: nameInput => {
+                        if  (isNaN(nameInput)) {
+                            console.log ("Please enter the teamMember's ID!")
+                            return false; 
+                        } else {
+                            return true;
+                        }
+                    }
+                },
+
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: "Please enter the engineer's github username.",
+                    when: (input) => input.jobRank === "Engineer",
+                    validate: nameInput => {
+                        if (nameInput ) {
+                            return true;
+                        } else {
+                            console.log ("Please enter the engineer's github username!")
+                        }
+                    }
+                },
+
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: "Please enter the intern's school",
+                    when: (input) => input.jobRank === "Intern",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log ("Please enter the intern's school!")
+                        }
+                    }
+                },
+
+                {
+                    type: 'confirm',
+                    name: 'addAnother',
+                    message: 'Add another team member?',
+                    default: false
+                }
+
+                ])
+            
+            
+                .then(teamMemberData => {
+                    // data for teamMember types 
+            
+                    let { teamMemberName, teamMemberID, teamMemberEmail, jobRank, github, school, addAnother } = teamMemberData; 
+                    let teamMember; 
+            
+                    if (jobRank === "Engineer") {
+                        teamMember = new Engineer (teamMemberName, teamMemberID, teamMemberEmail, github);
+            
+                        console.log(teamMember);
+            
+                    } else if (jobRank === "Intern") {
+                        teamMember = new Intern (teamMemberName, teamMemberID, teamMemberEmail, school);
+            
+                        console.log(teamMember);
+                    }
+            
+                    teamMembers.push(teamMember); 
+            
+                    if (addAnother) {
+                        return teamMemberPrompt(teamMembers); 
+                    } else {
+                        return teamMembers;
+                    }
+                })            
+            
+            
+            
+            }
+            
+            const writeFile = data => {
+                fs.writeFile('../sample/index.html', data, err => {
+                    // if there is an error 
+                    if (err) {
+                        console.log(err);
+                        return;
+                    // when the profile has been created 
+                    } else {
+                        console.log("Your team profile has been successfully created! Please check out the index.html")
+                    }
+                })
+            }; 
+
+            managerPrompt()
+            .then(teamMemberPrompt)
+            .then(teamMembers => {
+              return generateHTML(teamMembers);
+            })
+            .then(pageHTML => {
+              return writeFile(pageHTML);
+            })
+            .catch(err => {
+           console.log(err);
+            }); 
---
+++ tests/team-member.test.js
@@ -0,0 +1,48 @@
+const TeamMember = require('../lib/team-member');
+
+describe ('TeamMember', () => {
+    describe('Team Member object creation', () => {
+        it('should create an employee object', () => {
+            //Arrange
+            const teamMember = new TeamMember('Jaraad', 24, 'jhines19@gmail.com');
+            //Act
+
+            //Assert
+            expect(teamMember.name).toEqual(expect.any(String));
+            expect(teamMember.id).toEqual(expect.any(Number));
+            expect(teamMember.email).toEqual(expect.any(String));
+        })
+    })
+
+    describe('Retrieve name from Team Member object', () => {
+        it('should get name from getName() method', () => {
+            const teamMember = new TeamMember('Jaraad', 24, 'jhines19@gmail.com');
+
+            expect(teamMember.getName()).toEqual(expect.any(String));
+        });
+    });
+
+    describe('Retrieve id from Team Member object', () => {
+        it('should get id from getId() method', () => {
+            const teamMember = new TeamMember('Jaraad', 24, 'jhines19@gmail.com');
+
+            expect(teamMember.getId()).toEqual(expect.any(Number));
+        });
+    });
+
+    describe('Retrieve email from Team Member object', () => {
+        it('should get email from getEmail() method', () => {
+            const teamMember = new TeamMember('Jaraad', 24, 'jhines19@gmail.com');
+
+            expect(teamMember.getEmail()).toEqual(expect.stringContaining(teamMember.email.toString()));
+        });
+    });
+
+    describe('Retrieve role from Team Member object', () => {
+        it('should get emil from getRole() method', () => {
+            const teamMember = new TeamMember('Jaraad', 24, 'jhines19@gmail.com');
+
+            expect(teamMember.getRole()).toEqual("TeamMember");
+        })
+    })
+})
